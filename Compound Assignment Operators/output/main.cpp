2b3093676cc8f85144f038838da09acca6a470ba
Obeng Benedict Agyeman <nanaobengagyeman752@gmail.com>
2026-07-29 19:31:04 +0000

    pushing all my work from last week to today

---
+++ Compound Assignment Operators/output/main.cpp
@@ -0,0 +1,35 @@
+// this includes the iostream library
+#include <iostream>
+
+
+int main(){
+  
+    
+    int value {42};
+    std::cout<<"The value is : "<<value<<std::endl;
+
+    std::cout<<std::endl;
+    value +=5;// value {45} is the same then Add 5
+    std::cout<<"The value is increased by 5 :"<<value<<std::endl;
+    
+
+    std::cout<<std::endl;
+    value -= 5;
+    std::cout<<"The value check equality and decrese it by 5: " <<value << std::endl;
+
+    std::cout<<std::endl;
+    value /=3;
+    std::cout<<"The value check equality and devide by 3: " <<value << std::endl;
+
+    
+     std::cout<<std::endl;
+    value *=3;
+    std::cout<<"The value check equality and multiply by 3: " <<value << std::endl;
+
+     std::cout<<std::endl;
+    value %=3;
+    std::cout<<"The value check equality and devide by 3 returning the remainder: " <<value << std::endl;
+
+    return 0;
+    // the program ends here
+}
