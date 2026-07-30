31ec883a9f28c4dbb2f512d39fa2ddb0d249dc16
Obeng Benedict Agyeman <nanaobengagyeman752@gmail.com>
2026-07-30 01:38:19 +0000

    Added main file

--- Compound Assignment Operators/output/main.cpp
+++ Compound Assignment Operators/output/main.cpp
@@ -8,10 +8,10 @@ int main(){
     int value {42};
     std::cout<<"The value is : "<<value<<std::endl;
 
-    std::cout<<std::endl;
+    std::cout<<std::boolalpha;
     value +=5;// value {45} is the same then Add 5
     std::cout<<"The value is increased by 5 :"<<value<<std::endl;
-    
+    std::cout<<"value is > 50: " << (value > 50) <<std::endl;
 
     std::cout<<std::endl;
     value -= 5;
