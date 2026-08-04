f98a3a5cb13001461c85e1312aeafaf51c6d9536
Obeng Benedict Agyeman <nanaobengagyeman752@gmail.com>
2026-08-04 10:58:11 +0000

    Now almost done with the formated table

--- Output Formating/output/main.cpp
+++ Output Formating/output/main.cpp
@@ -1,6 +1,6 @@
 // this includes the iostream library
 #include <iostream>
-
+#include <iomanip>//this function sets the width of the table and it content
 
 int main(){
     // this is going to print out "hello world"
@@ -27,7 +27,7 @@ std::cout<<std::endl;
 
 //std::flushes the output to it's final destination 
 std::cout<<"My God never fails me"<<std::flush;
-
+std::cout<<"-------------------"<<std::endl;
 std::cout<<std::endl;
 //==============================================
 //Trying something crazy
@@ -40,7 +40,16 @@ std::cout<<"Joe  "<<"Ball "<<"21"<<std::endl;
 std::cout<<"Josh  "<<" Carr "<<"27"<<std::endl;
 std::cout<<"Izaiah  "<<"Robinson "<<"30"<<std::endl;
 
+std::cout<<std::endl;
 
+std::cout<<" Formated Table :"<<std::endl;
+std::cout << std::setw(10)<<"Firstname  "<< std::setw(10)<< "Last Name  " << std::setw(10)<< "Age"<<std::endl;
+ std::cout<< std::setw(10)<<"Daniel   "<< std::setw(10)<<"Gray  "<< std::setw(10)<<"25"<<std::endl;
+std::cout<< std::setw(10)<<"Stanley "<< std::setw(10)<<"wood  "<< std::setw(10)<<"33  "<<std::endl;
+std::cout<<"Jordan "<< std::setw(10)<<"Parker "<< std::setw(10)<<"45"<<std::endl;
+std::cout<< std::setw(10)<<"Joe  "<< std::setw(10)<<"Ball "<< std::setw(10)<<"21"<<std::endl;
+std::cout<< std::setw(10)<<"Josh  "<< std::setw(10)<<" Carr "<< std::setw(10)<<"27"<<std::endl;
+std::cout<< std::setw(10)<<"Izaiah  "<< std::setw(10)<<"Robinson "<< std::setw(10)<<"30"<<std::endl;
 
 
     return 0;
