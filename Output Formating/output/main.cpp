cecf26088190053ea48779134cd492b39621ac31
Obeng Benedict Agyeman <nanaobengagyeman752@gmail.com>
2026-08-04 10:11:24 +0000

    print out characters on different lines

--- Output Formating/output/main.cpp
+++ Output Formating/output/main.cpp
@@ -4,11 +4,21 @@
 
 int main(){
     // this is going to print out "hello world"
-    std::cout << "hello " << std::endl;
-    std::cout <<"World"<<std::endl;
+    std::cout << "hello " ;
+    std::cout <<"World";
 
 
-    
+std::cout<<std::endl;
+std::cout<<"---------------------------"<<std::endl;
+// and to print it on the same line 
+
+std::cout<<"hello"<<std::endl;
+std::cout<<"World"<<std::endl;
+
+std::cout<<std::endl;
+std::cout<<"Hello\n";
+std::cout<<"World\n";
+
     return 0;
     // the program ends here
 }
